0d49be378a68ded77328f984d450580d03e0b354
zadragon <mase0804@naver.com>
2023-03-27 23:05:40 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://sparta:test@cluster0.ottleza.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+
+db = client.miniProject
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/guestbook", methods=["POST"])
+def guestbook_post():
+   
+    
+    return jsonify({'msg': '저장 완료!'})
+
+@app.route("/guestbook", methods=["GET"])
+def guestbook_get():
+    return jsonify({'msg': '로드 완료!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5001, debug=True)
